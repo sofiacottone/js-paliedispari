82101b6aae90b78caa592e3c88c687edb6314a5b
sofiacottone <cottonesofia@outlook.it>
2024-03-14 15:19:43 +0100

    feat: add steps

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -3,3 +3,16 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+
+// chiedo all'utente di scegliere tra pari e dispari
+
+// chiedo all'utente di inseriire un numero tra 1 e 5
+
+// genero un numero random tra 1 e 5 per il computer (funzione)
+
+// sommo i due numeri
+
+// stabilisco se la somma dei due numeri è pari o dispari (funzione)
+
+// dichiaro il vincitore
