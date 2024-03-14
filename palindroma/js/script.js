1e4ca32d0b87e4ac97128726cdfea73a7367ca4b
sofiacottone <cottonesofia@outlook.it>
2024-03-14 16:54:27 +0100

    feat: add if function to print userMessage after reverting userWord to determine if it is a palindrome word

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,12 +3,23 @@
 
 
 // chiedo all'utente di inserire una parola
+const userWord = prompt('Inserisci una parola per sapere se è palindroma');
+console.log('user word:', userWord);
 
 // per capire se è palindroma:
 // giro una parola al contrario
+let userRevertedWord = revertWord(userWord);
+console.log('user word reverted:', userRevertedWord);
+
 // se la parola è uguale alla parola al contrario allora è palindroma
-
-
+let userMessage;
+if (userWord === userRevertedWord) {
+    userMessage = 'La parola è palindroma!';
+} else {
+    userMessage = 'La parola non è palindroma!';
+}
+console.log('palindroma?', userMessage)
+alert(userMessage);
 
 // #region FUNCTIONS
 
